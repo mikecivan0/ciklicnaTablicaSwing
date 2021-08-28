5df7f265762d8ebc8886653cf374b903ddac798a
Ivan Mikec <mikecivan0@gmail.com>
2021-08-28 20:19:08 +0200

    dodane datoteke koje nisu bile ucitane prvi puta

---
+++ CiklicnaTablica/src/main/java/hr/edunova/ciklicnatablica/Start.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hr.edunova.ciklicnatablica;
+
+
+public class Start {
+    public static void main(String[] args) {
+        new Prozor().setVisible(true);
+    }
+}
